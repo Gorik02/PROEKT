29b74d0eb034692845c5226f556057f1897056eb
Gorik02 <cheremvaldmit@gmail.com>
2024-10-16 18:58:39 +0300

    Add files via upload

--- gbnjy.py
+++ gbnjy.py
@@ -5,6 +5,9 @@
 ##    print("Вам ещё рано!")
 ##    if a==14:
 ##        print("Вам 14 лет поздравляю!")
+
+
+
 #№2##a=int(input())
 ##while a!='secret':
 ##    print("Неверный пароль!")
@@ -12,22 +15,23 @@
 ##    print("Добро пожаловать :)")
 ##s=0
 
-проверка числа или буквы любого алфавита
-a=input()
-if a.isdigit():
-    print('число')
-elif a.isalpha():
-    print('Буква')
-else:
-    print('Ни число ни буква')
+##проверка числа или буквы любого алфавита
+##a=input()
+##if a.isdigit():
+##    print('число')
+##elif a.isalpha():
+##    print('Буква')
+##else:
+##    print('Ни число ни буква')
 
+##s=0
 ##a=float(input())
 ##if a>1000:
-##    a=a-a*0,01
+##    a=a-a*0.01
 ##s+=a
 ##print(s)
 ##if 500<=a>=1000:
-##    a=a-a*0,05
+##    a=a-a*0.05
 ##s+=a
 ##print(s)
 ##if a<500:
@@ -35,6 +39,21 @@ else:
 
 
 
+##k=0
+##a,b,c,d,e= map(int, input().split())
+##k=a+b+c+d+e
+##print(k)
+
+
+##a=int(input())
+##s=1
+##for i in range(2, a+1):
+##    s*=i
+##print(s)
+
+
+
+#Проверку на простое число забыл
 
 
 
@@ -59,4 +78,24 @@ else:
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
